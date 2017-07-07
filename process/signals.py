6b9f09d61cc80c1662e193d6a608af237e772cd4
toast38coza <info@38.co.za>
2017-07-07 12:30:27 +0200

    Build the task args based on the payload provided

--- process/signals.py
+++ process/signals.py
@@ -6,13 +6,34 @@ from django.utils.module_loading import import_string
 from django.dispatch import receiver
 from .registry import PROCESS_REGISTRY
 from .models import Process, Task
-import json, yaml
+import json, yaml, sys
 
-def validate_payload(self, data, payload_definition):
+def validate_payload(data, payload_definition):
     pass
 
-def extract_arguments(self, data, expected_payload):
-    pass
+def recursive_lookup(key, data, default = None):
+    bits = key.split('.')# [foo,bar,baz]
+    try:
+        new_data = data.get(bits[0], default)
+        if new_data is None:
+            raise(KeyError('No value found for {} ({})'.format(key, bits[0])))
+        # print ('{}: {}: {}'.format(len(bits), bits, new_data))
+        if len(bits) == 1:
+            return new_data
+        else:
+            new_key = ('.').join(bits[1:])
+            return recursive_lookup(new_key, new_data)
+    except AttributeError:
+        return data
+
+def extract_arguments(data, payload_definition):
+    args = {}
+    data = {'payload': data}
+
+    for key, lookup in payload_definition.items():
+        args[key] = recursive_lookup(lookup, data)
+
+    return args
 
 def execute_tasks(instance, tasks, async=False):
 
@@ -21,20 +42,24 @@ def execute_tasks(instance, tasks, async=False):
 
     for task in tasks:
         task_name = task.get('task')
+        task_args = task.get('args', {})
         task_object = Task.objects.create(
                         process=instance,
                         task_id=task_name)
 
         try:
-            data = json.loads(instance.request_data)
+            payload = json.loads(instance.request_data)
+            data = extract_arguments(payload, task_args)
             result = import_string(task_name)(**data)
             task_object.result_data=json.dumps(result)
             task_object.status = 'C'
-        except:
+        except Exception as err:
             task_object.status = 'F'
+            print(err)
+            print(sys.exc_info())
+            import ipdb;ipdb.set_trace()
             # put the error message on the result_data
         task_object.save()
-
 
 @receiver(post_save, sender=Process, dispatch_uid="process.signals.process_created")
 def process_created(sender, instance, created, **kwargs):
@@ -52,8 +77,8 @@ def process_created(sender, instance, created, **kwargs):
         tasks_now = process_data.get('tasks', [])
         execute_tasks(instance, tasks_now)
 
-        async_tasks_now = process_data.get('tasks_async', [])
-        execute_tasks(instance, async_tasks_now, async=True)
+        # async_tasks_now = process_data.get('tasks_async', [])
+        # execute_tasks(instance, async_tasks_now, async=True)
 
         # schedule tasks:
 
--- process/tests/test_signals.py
+++ process/tests/test_signals.py
@@ -1,20 +1,80 @@
 from django.test import TestCase
 from ..models import Process, Task
+from ..signals import extract_arguments, recursive_lookup
+import json
 
-"""
 class ProcessSignalTestCase(TestCase):
 
     def setUp(self):
-        self.process = Process.objects.create(process_id='hello-world')
+        payload = '{"message": "testing"}'
+        self.process = Process.objects.create(
+                        process_id='hello-world',
+                        request_data=payload)
         self.tasks = Task.objects.filter(process_id=self.process.id)
 
     def test_process_creates_tasks(self):
         assert len(self.tasks) == 1
 
+    def test_task_executes(self):
+        task = self.tasks.first()
+        result = json.loads(task.result_data).get('result')
+        assert result == 'testing', \
+            'Expected result to be testing. got: {}'.format(result)
+
+'''
     def test_creates_correct_task(self):
         assert self.tasks[0].task_id == 'process.tasks.io.say_hello'
 
     def test_updates_task_status(self):
         assert self.tasks[0].status == 'C',\
             'Expected task to complete successfull. Result was: {}'.format(self.tasks[0].__dict__)
-"""
+'''
+
+class RecursiveLookupTestCase(TestCase):
+
+    def setUp(self):
+        self.data = {'payload': {'foo': {'bar': {'baz': 'bus'}}}}
+
+    def test_nested_lookup(self):
+        key = 'payload.foo.bar.baz'
+        result = recursive_lookup(key, self.data)
+        assert result == 'bus', 'Expected bus. Got: {}'.format(result)
+
+    def test_base_lookup(self):
+        key = 'payload'
+        result = recursive_lookup(key, self.data)
+        assert result == self.data.get('payload'), \
+            'Got: {}'.format(result)
+
+    def test_middle_lookup(self):
+        key = 'payload.foo.bar'
+        result = recursive_lookup(key, self.data)
+        assert result == {'baz': 'bus'}, \
+            'Expected {\'baz\': \'bus\'}. Got: {}'.format(result)
+
+    def test_incorrect_key_throws_keyerror(self):
+        key = 'key.does.not.exist'
+        with self.assertRaises(Exception) as context:
+            recursive_lookup(key, self.data)
+
+    def test_can_specify_default(self):
+        key = 'key.does.not.exist'
+        result = recursive_lookup(key, self.data, 'default')
+        assert result == 'default'
+
+class ExtractArgsTestCase(TestCase):
+
+    def test_simple_non_nested_argument(self):
+        payload = { 'message': 'testing' }
+        payload_definition = {'message': 'payload.message'}
+
+        result = extract_arguments(payload, payload_definition)
+        assert result['message'] == 'testing', \
+            'Expected foo to be bar. Actual response: {}'.format(result)
+        assert result.get('baz', None) is None
+
+    def test_passing_the_whole_payload(self):
+        pass
+
+    def test_passing_nested_argument(self):
+        pass
