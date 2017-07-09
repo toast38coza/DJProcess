7235bbffe25e540acc9b63f7a5fb57ee7c6e1acb
kangwa <kangwa2003@gmail.com>
2017-07-09 23:26:07 +0200

    added data types dict

--- djprocess/settings.py
+++ djprocess/settings.py
@@ -122,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATA_TYPES = {
+'string': str,
+'number': int,
+'object': dict,
+'array': list,
+'list': list,
+'boolean': bool
+}
